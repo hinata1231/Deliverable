67691dd07fce39acdb606c6d91fcfbca9f474020
hinata1231 <k21kk044@st.kyusan-u.ac.jp>
2023-08-25 17:07:02 +0900

    先生の紹介ページ作成①

---
+++ app/Http/Controllers/Teacher_IntroduceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teacher;
+use Illuminate\Http\Request;
+
+
+class Teacher_IntroduceController extends Controller
+{
+    public function teacher_introduce(Teacher $post)
+    {
+       return view('classe/teacher_introduce')->with(['posts' => $post->get()]); 
+    }
+}
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Teacher_IntroduceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function() {
-    return view('posts.classe');
-});
+Route::get('/', [Teacher_IntroduceController::class, 'teacher_introduce']);
